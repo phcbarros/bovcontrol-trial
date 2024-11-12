ba7069ebe96b097572d46c1fc793917d0cc31df3
phcbarros <paulohcbarros@outlook.com>
2024-11-11 21:15:30 -0300

    chore: instalar styled-components

---
+++ src/screens/Home/styles.ts
@@ -0,0 +1,15 @@
+import styled from 'styled-components/native'
+
+export const Container = styled.View`
+  flex: 1;
+  background-color: #fff;
+  align-items: center;
+  justify-content: center;
+`
+
+export const Title = styled.Text`
+  font-size: 24px;
+  font-weight: bold;
+  margin-bottom: 20px;
+  color: #000;
+`
